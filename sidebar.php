cf2b640ddc57fc91c874e9a2c5c1f5d98c44e0fd
KAZU <kazu.since2019@gmail.com>
2024-08-30 02:30:37 +0900

    first commit

---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<section>
+            <h2>個別指導ホームページ作成教室</h2>
+            <p><img src="<?php echo get_template_directory_uri(); ?>/images/webst8-banar.png" alt="バナー画像" /></p>
+          </section>
+          <section>
+            <h2>YouTubeチャンネル開設</h2>
+            <p><img src="<?php echo get_template_directory_uri(); ?>/images/webst8-youtube.png" alt="WEBST8のYouTubeチャンネル画像" /></p>
+          </section>
